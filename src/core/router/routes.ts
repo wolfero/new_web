5edd0f02083583ac824288acda7dfbb0c6687e67
Wolfero <ferorock44@gmail.com>
2023-08-22 13:15:17 +0100

    ✨ add routes

---
+++ src/core/router/routes.ts
@@ -0,0 +1,9 @@
+import { BaseRoute } from "./routes.vm";
+
+export const routes: BaseRoute[] = [
+    { name: 'Home', href: '/' },
+    { name: 'Blog', href: '/blog' },
+    { name: 'Projects', href: '/projects' },
+    { name: 'About', href: '/about' },
+    { name: 'Contact', href: '/contact' },
+];
---
+++ src/core/router/routes.vm.ts
@@ -0,0 +1,4 @@
+export interface BaseRoute {
+    name: string;
+    href: string;
+}
