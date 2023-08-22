4c59c3d8b38012fac0c765162f0a69d7f8f76926
Wolfero <ferorock44@gmail.com>
2023-08-22 13:59:33 +0100

    🗃️ add social links

---
+++ data/constants/socialLinks.ts
@@ -0,0 +1,11 @@
+const gmail = "frantisek.klucar4@gmail.com"
+const linkedIn = "https://www.linkedin.com/in/frantisek-klucar/"
+const github = "https://github.com/wolfero"
+const webRepository = `${github}/www.wolfero.dev`
+
+export const socialLinks = {
+    gmail,
+    linkedIn,
+    github,
+    webRepository
+}
