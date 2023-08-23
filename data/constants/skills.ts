81ad4b97a9caf389ee84baf92dcd8f801e119730
Wolfero <ferorock44@gmail.com>
2023-08-23 08:58:37 +0100

    🗃️ add skills constants

---
+++ data/constants/skills.ts
@@ -0,0 +1,37 @@
+export const skillsTags = [
+    {
+        type: "html",
+        title: "HTML",
+        color: "#E56027"
+    },
+    {
+        type: "css",
+        title: "CSS",
+        color: "#2760E5"
+    },
+    {
+        type: "es6",
+        title: "JavaScript",
+        color: "#EAD41C"
+    },
+    {
+        type: "sass",
+        title: "Sass",
+        color: "#C36291"
+    },
+    {
+        type: "react",
+        title: "React",
+        color: "#09CEF2"
+    },
+    {
+        type: "git",
+        title: "Git",
+        color: "#E44D30",
+    },
+    {
+        type: "csharp",
+        title: "C #",
+        color: "#753091",
+    }
+]
