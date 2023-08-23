cd4bceeb93db7824f6384a076b39385d3f7e21d2
Wolfero <ferorock44@gmail.com>
2023-08-23 08:48:18 +0100

    ✏️ fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi my name is Fero, which is short for Frantisek. I`&apos;`m Backend specialist
-					at{' '}
+					Hi my name is Fero, which is short for Frantisek. I&apos;m Backend specialist at{' '}
 					<Link
 						target="_blank"
 						href="https://pwc.com"
@@ -24,7 +23,7 @@ export default function Home() {
 						PWC
 					</Link>
 					<br />
-					and I recently finished my master`&apos;`s degree in Frontend at{' '}
+					and I recently finished my master&apos;s in Frontend at{' '}
 					<Link
 						target="_blank"
 						href="https://lemoncode.net/"
