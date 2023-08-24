b86e58817f602315f113a30808e5a990892f554e
Wolfero <ferorock44@gmail.com>
2023-08-24 12:48:37 +0100

    🔍 add siteMetadata

---
+++ data/constants/siteMetadata.ts
@@ -0,0 +1,25 @@
+export const siteMetadata = {
+    title: {
+        default: 'Frantisek Klucar',
+        template: 'Frantisek Klucar',
+    },
+    description: 'Backend specialist at PWC and self-taught Frontend trainee.',
+    openGraph: {
+        title: 'Frantisek Klucar',
+        description: 'Backend specialist at PWC and self-taught Frontend trainee.',
+        url: 'https://wwolfero.dev',
+        siteName: 'Frantisek Klucar',
+        images: [
+            {
+                url: '/assets/wolfero.png',
+                width: 1920,
+                height: 1080,
+            },
+        ],
+        locale: 'es-ES',
+        type: 'website',
+    },
+    icons: {
+        shortcut: '/assets/wolfero.png',
+    },
+}
