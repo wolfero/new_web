09cf336a44a9d0af474e775882ad135776467efc
Wolfero <ferorock44@gmail.com>
2023-08-24 12:51:42 +0100

    🥅 add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import '@/../globals.css';
+import React from 'react';
+import Link from 'next/link';
+import { TbError404 } from 'react-icons/tb';
+import { Layout } from '@/common/components';
+
+const Custom404 = () => {
+	return (
+		<Layout>
+			<div className="flex items-center justify-center ">
+				<div className=" p-8 rounded-lg text-center">
+					<TbError404 className="text-6xl mb-4" />
+					<h1 className="text-2xl font-bold mb-4">
+						Página no encontrada o ya no existe 🙂
+					</h1>
+					<p className="mb-4">Siento los inconvenientes que esto le pueda causar.</p>
+					<Link href="/">
+						<p className="text-lightGreen hover:underline">
+							Volver a la página principal
+						</p>
+					</Link>
+				</div>
+			</div>
+		</Layout>
+	);
+};
+
+export default Custom404;
